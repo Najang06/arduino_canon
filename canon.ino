947c258818a354b325f3b33a01f15125b3df4309
Najang06 <133004461+Najang06@users.noreply.github.com>
2024-04-07 23:37:59 +0900

    add canon.ino
    add file "canon.ino"

---
+++ canon.ino
@@ -0,0 +1,458 @@
+float do_hh = 1046.5;
+float si_h = 987.8;
+float ra_h = 880.0;
+float sol_h = 783.9;
+float pa_h = 698.5;
+float mi_h = 659.3;
+float re_h = 587.3;
+float do_h = 523.2;
+float si = 493.9;
+float ra = 440.0;
+float sol = 392.0;
+float pa = 349.6;
+float mi = 329.6;
+float re = 293.7;
+float do_ = 261.6;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12, OUTPUT); //speaker1
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+delay(1000);
+  
+tone(12, mi_h);
+delay(1500);
+
+tone(12, re_h);
+delay(1500);
+
+tone(12, do_h);
+delay(1500);
+
+tone(12, si);
+delay(1500);
+
+tone(12, ra);
+delay(1500);
+
+tone(12, sol);
+delay(1500);
+
+tone(12, ra);
+delay(1500);
+
+tone(12, si);
+delay(1500);
+  
+tone(12, do_h);
+delay(1500);
+
+tone(12, si);
+delay(1000);
+
+tone(12, sol);
+delay(1000);
+
+tone(12, do_h);
+delay(1000);
+
+tone(12, mi_h);
+delay(1000);
+
+tone(12, sol_h);
+delay(1000);
+
+tone(12, si_h);
+delay(1000);
+
+tone(12, pa_h);
+delay(1000);
+
+tone(12, do_h);
+delay(1000);
+
+tone(12, mi_h);
+delay(1000);
+
+tone(12, sol);
+delay(1000);
+
+tone(12, pa);
+delay(1000);
+
+tone(12, do_h);
+delay(1000);
+
+tone(12, si);
+delay(1000); ///////
+
+tone(12, do_h);
+delay(500);
+
+tone(12, re_h);
+delay(500);
+  
+  
+  
+//dodol
+tone(12, sol_h);
+delay(500);
+
+tone(12, mi_h);
+delay(250);
+  
+tone(12, pa_h);
+delay(250);
+
+tone(12, sol_h);
+delay(500);
+
+tone(12, mi_h);
+delay(250);
+  
+tone(12, pa_h);
+delay(250);
+
+tone(12, sol_h);
+delay(250);
+
+tone(12, sol);
+delay(250);
+
+tone(12, ra);
+delay(250);
+
+tone(12, si);
+delay(250);
+
+tone(12, do_h);
+delay(250);
+
+tone(12, re_h);
+delay(250);
+  
+tone(12, mi_h);
+delay(250);
+
+tone(12, pa_h);
+delay(250);
+
+tone(12, mi_h);
+delay(500);
+////
+tone(12, do_h);
+delay(250);
+  
+tone(12, re_h);
+delay(250);
+  
+tone(12, mi_h);
+delay(500);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, re_h);
+delay(250);
+  
+tone(12, mi_h);
+delay(250);//
+  
+  
+tone(12, mi);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, sol);
+delay(250);
+  
+tone(12, ra);
+delay(250);
+  
+tone(12, sol);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, si);
+delay(250);//
+  
+  
+tone(12, ra);
+delay(500);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, ra);
+delay(500);//
+  
+//
+tone(12, sol);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, sol);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, mi);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, sol);
+delay(250);
+  
+tone(12, ra);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(500);//
+  
+  
+tone(12, ra);
+delay(250);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(500);
+  
+// 1
+tone(12, si);
+delay(250);
+  
+tone(12, ra);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, re_h);
+delay(250);
+  
+tone(12, mi_h);
+delay(250);
+  
+tone(12, pa_h);
+delay(250);
+  
+tone(12, sol_h);
+delay(250);
+  
+  
+//
+noTone(12);
+delay(50);
+//dodol
+tone(12, sol_h);
+delay(500);
+
+tone(12, mi_h);
+delay(250);
+  
+tone(12, pa_h);
+delay(250);
+
+tone(12, sol_h);
+delay(500);
+
+tone(12, mi_h);
+delay(250);
+  
+tone(12, pa_h);
+delay(250);
+
+tone(12, sol_h);
+delay(250);
+
+tone(12, sol);
+delay(250);
+
+tone(12, ra);
+delay(250);
+
+tone(12, si);
+delay(250);
+
+tone(12, do_h);
+delay(250);
+
+tone(12, re_h);
+delay(250);
+  
+tone(12, mi_h);
+delay(250);
+
+tone(12, pa_h);
+delay(250);
+
+tone(12, mi_h);
+delay(500);
+////
+tone(12, do_h);
+delay(250);
+  
+tone(12, re_h);
+delay(250);
+  
+tone(12, mi_h);
+delay(500);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, re_h);
+delay(250);
+  
+tone(12, mi_h);
+delay(250);//
+  
+  
+tone(12, mi);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, sol);
+delay(250);
+  
+tone(12, ra);
+delay(250);
+  
+tone(12, sol);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, si);
+delay(250);//
+  
+  
+tone(12, ra);
+delay(500);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, ra);
+delay(500);//
+  
+//
+tone(12, sol);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, sol);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, mi);
+delay(250);
+  
+tone(12, pa);
+delay(250);
+  
+tone(12, sol);
+delay(250);
+  
+tone(12, ra);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(250);//
+  
+  
+tone(12, ra);
+delay(500);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(500);
+  
+  
+//2
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, re_h);
+delay(250);
+  
+tone(12, do_h);
+delay(250);
+  
+tone(12, ra);
+delay(250);
+  
+tone(12, si);
+delay(250);
+  
+tone(12, do_h);
+delay(1000);
+  
+noTone(12);
+}
